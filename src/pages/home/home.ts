9906485c0c4b34fb7b459700b1478c0f372b20ad
Audun Larsen <larsen@xqus.com>
2018-08-13 21:58:40 +0200

    Adds buttons and modals

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController, Platform, NavParams, ViewController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,60 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
 
+  }
+
+  openModal(typeNum) {
+    let modal = this.modalCtrl.create(ModalContentPage, typeNum);
+    modal.present();
+  }
+}
+
+@Component({
+  template: `
+<ion-header>
+  <ion-toolbar>
+    <ion-title>
+      {{type.name}}
+    </ion-title>
+    <ion-buttons start>
+      <button ion-button (click)="dismiss()">
+        <span ion-text color="primary" showWhen="ios">Cancel</span>
+        <ion-icon name="md-close" showWhen="android, windows"></ion-icon>
+      </button>
+    </ion-buttons>
+  </ion-toolbar>
+</ion-header>
+<ion-content>
+
+</ion-content>
+`
+})
+export class ModalContentPage {
+  character;
+
+  constructor(
+    public platform: Platform,
+    public params: NavParams,
+    public viewCtrl: ViewController
+  ) {
+    var types = [
+      {
+        name: 'Normal',
+      },
+      {
+        name: 'Fire',
+      },
+      {
+        name: 'Water',
+      }
+    ];
+    this.type = types[this.params.get('typeNum')];
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
   }
 
 }
